1586ef1f2b8dfc132760e8ffb0b98cf71d2e0aac
747618 <subhamkushwahagd@gmail.com>
2024-07-29 16:22:28 +0530

    initial commit

---
+++ src/test/java/com/delieveryboy/DeliveryboyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.delieveryboy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryboyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
